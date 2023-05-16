ca7bc3c94d8debf6977c570eaf5dcac5fc313709
Mohmadaman Vhora <mohmadaman@vhora.com>
2023-05-16 18:58:30 +0530

    TE7-T129:Rails:Complete custom user login and authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: [:new, :create]
+  before_action :redirect_if_authenticated, only: [:new, :create]
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user.present? && @user.authenticate(params[:user][:password])
+      if params[:user][:remember_me] == '1'
+        cookies.permanent[:remember_token] = @user.remember_token
+      else
+        cookies[:remember_token] = @user.remember_token
+      end
+      redirect_to root_path, flash: { notice: "Logged in successfully" }
+    else
+      redirect_to sessions_path, flash: { alert: "Invalid username or password"}
+    end
+  end
+
+  def destroy
+    cookies.delete(:remember_token)
+    redirect_to new_session_path, flash: { notice: "Logged out successfully" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'tweets#index'
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
