59b32bfaa7692d102cf0c86020710303a21dc218
Mohmadaman Vhora <mohmadaman@vhora.com>
2023-06-19 19:14:37 +0530

    TE7-T135:Rails:Complete all practicals of securing rails applications

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < ApplicationController
-  skip_before_action :authenticate_user, only: [:new, :create]
-  before_action :redirect_if_authenticated, only: [:new, :create]
+  skip_before_action :authenticate_user, only: %i[new create]
+  before_action :redirect_if_authenticated, only: %i[new create]
 
   def new
     @user = User.new
@@ -9,7 +9,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path, flash: { notice: "Registration completed successfully"}
+      redirect_to new_session_path, flash: { notice: 'Registration completed successfully' }
     else
       render :new, status: :unprocessable_entity
     end
@@ -18,6 +18,6 @@ class RegistrationsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :credit_card, :cvv)
   end
 end
